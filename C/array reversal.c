b4529c8f7670946e676a458fde07771abb74bd1e
Crypt0 <84045971+Crypt00o@users.noreply.github.com>
2022-04-19 13:06:54 -0400

    Create array reversal.c

---
+++ C/array reversal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, *arrrev;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    arrrev = (int*) malloc(num * sizeof(int));
+    for(int i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+for (int i=0,j=num-1; i <num ;i++,j--){
+    arrrev[i]=arr[j];
+}
+for(int i = 0; i < num; i++)
+        *(arr+i)=*(arrrev+i);
+
+
+    /* Write the logic to reverse the array. */
+
+    for(int i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+        free(arr);free(arrrev);
+    return 0;
+}
